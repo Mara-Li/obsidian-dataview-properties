7d61ee6daabfb97202b7e4c996ecf7d80ba957e2
Spoc Web <10584012+SpocWeb@users.noreply.github.com>
2026-01-06 20:11:11 +0100

    removed unnecessary skipPattern

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -68,12 +68,6 @@ export interface DataviewPropertiesSettings {
 		 * @default {{key}} = `= this.{{prefix}}{{key}}`
 		 */
 		template: string;
-		/**
-		 * Regular expression pattern to skip replacement when the inline field value matches.
-		 * Useful for skipping fields that already contain dataview expressions.
-		 * @default ^=|^\\$=
-		 */
-		skipPattern: string;
 	};
 	dataviewOptions: {
 		durationFormat: DurationOptions;
@@ -145,7 +139,6 @@ export const DEFAULT_SETTINGS: DataviewPropertiesSettings = {
 	replaceInlineFieldsWith: {
 		enabled: false,
 		template: "{{key}} = `= this.{{prefix}}{{key}}`",
-		skipPattern: "^=|^\\$=",
 	},
 	dataviewOptions: {
 		durationFormat: {
